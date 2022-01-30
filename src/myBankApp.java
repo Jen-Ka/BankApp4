a4cbcd13ab1007bdcc78ad6013ef79495f18c07a
Jen-Ka <jenka.makarova@gmail.com>
2022-01-30 19:33:28 +0300

    Добавлены новые персоналы

--- src/myBankApp.java
+++ src/myBankApp.java
@@ -10,26 +10,24 @@ public class myBankApp {
         Client client = new Client("Иван", "Петров", 35, "1111 234325");
 
         client.giveMeTalon();
-        client.giveMeInfoAboutCredit();
-        client.giveMeContract();
-        client.giveMeMoney();
 
         Administrator administrator = new Administrator("Елена", "старший администратор");
-        administrator.setName("Елена");
-        administrator.setPosition("старший администратор");
 
         administrator.greetingClient();
         administrator.getTalon();
+        client.giveMeInfoAboutCredit();
 
         Consultant consultant = new Consultant("Мария", "консультант");
 
         consultant.greetingClient();
         consultant.getInfoAboutCredit();
+        client.giveMeContract();
 
         Manager manager = new Manager("Антон", "менеджер кредитного отдела");
 
         manager.greetingClient();
         manager.getContract();
+        client.giveMeMoney();
 
         Cashier cashier = new Cashier("Ольга", "старший кассир");
 
