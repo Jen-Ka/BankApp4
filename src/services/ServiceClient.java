dafb5a830fa2e88f75bed1d1916979d853f5b522
Jen-Ka <jenka.makarova@gmail.com>
2022-04-16 21:03:42 +0300

    ДЗ 11 подготовка к пулл-реквесту, попытка commit #4 to branch2Test
    Signed-off-by: Jen-Ka <jenka.makarova@gmail.com>

--- src/services/ServiceClient.java
+++ src/services/ServiceClient.java
@@ -12,6 +12,7 @@ public class ServiceClient {
 
         System.out.println("Test for branches - this for main " + t + " еее ");
         System.out.println("TEST#3");
+        System.out.println("TEST#4 - Pull request");
 
     }
 
