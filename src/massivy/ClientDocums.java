2912b4c49ddd94ba4e70a8bd3907d3eca09a8501
Jen-Ka <95135671+jen-ka@users.noreply.github.com>
2022-04-16 19:56:59 +0300

    ДЗ 11 подготовка к пулл-реквесту, добавлены 2 класса
    Signed-off-by: Jen-Ka <jenka.makarova@gmail.com>

---
+++ src/massivy/ClientDocums.java
@@ -0,0 +1,10 @@
+package massivy;
+
+import clients.Client;
+
+public class ClientDocums {
+
+    public static void main(String[] args) {
+        //String [] arrDocums ={"Паспорт", "Водительское удостоверение", "Загранпаспорт", "Справка";}
+    }
+}
