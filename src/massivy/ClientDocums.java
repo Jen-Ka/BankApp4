873c01413cd650455b03f5c6d36560b4b7c4bc9b
Jen-Ka <jenka.makarova@gmail.com>
2022-04-16 20:16:07 +0300

    ДЗ 11 подготовка к пулл-реквесту, попытка ветвления #2
    Signed-off-by: Jen-Ka <jenka.makarova@gmail.com>

--- src/massivy/ClientDocums.java
+++ src/massivy/ClientDocums.java
@@ -5,6 +5,8 @@ import clients.Client;
 public class ClientDocums {
 
     public static void main(String[] args) {
+
+        System.out.println("branches for github");
         //String [] arrDocums ={"Паспорт", "Водительское удостоверение", "Загранпаспорт", "Справка";}
     }
 }
