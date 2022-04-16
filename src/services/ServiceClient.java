c58d15d1e2ae301936df268b718a1b04d1945445
Jen-Ka <jenka.makarova@gmail.com>
2022-04-16 20:08:24 +0300

    ДЗ 11 подготовка к пулл-реквесту, добавлены 2 класса
    Signed-off-by: Jen-Ka <jenka.makarova@gmail.com>

--- src/services/ServiceClient.java
+++ src/services/ServiceClient.java
@@ -1,8 +1,17 @@
 package services;
 
 public class ServiceClient {
+
+
     public static void main(String[] args) {
-        System.out.println("Test for branches - this for main");
+        int t = 10;
+
+        do {
+            t++;
+        } while (t>=1);
+
+        System.out.println("Test for branches - this for main" + t);
+
     }
 
 }
